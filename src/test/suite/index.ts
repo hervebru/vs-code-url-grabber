a5bdba43d803eebc040b58bb2196f935dd1848b1
bruyere <herve.bruyere@nokia.com>
2026-03-27 21:57:32 +0100

    Environment changes

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,19 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as fs from 'fs';
+
+function findTestFiles(dir: string): string[] {
+	const results: string[] = [];
+	for (const entry of fs.readdirSync(dir, { withFileTypes: true })) {
+		const fullPath = path.join(dir, entry.name);
+		if (entry.isDirectory()) {
+			results.push(...findTestFiles(fullPath));
+		} else if (entry.name.endsWith('.test.js')) {
+			results.push(fullPath);
+		}
+	}
+	return results;
+}
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -11,28 +24,24 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
+	// Add files to the test suite
+	for (const f of findTestFiles(testsRoot)) {
+		mocha.addFile(f);
+	}
+
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
+		try {
+			// Run the mocha test
+			mocha.run(failures => {
+				if (failures > 0) {
+					e(new Error(`${failures} tests failed.`));
+				} else {
+					c();
+				}
+			});
+		} catch (err) {
+			console.error(err);
+			e(err);
+		}
 	});
 }
